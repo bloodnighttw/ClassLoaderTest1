614c9f373d697dcaa9d273de053813f5181ccd96
bloodnight <bbeenn1227@gmail.com>
2020-01-24 12:45:09 +0000

    mark improve

--- Main.java
+++ Main.java
@@ -1,8 +1,28 @@
+/*
+ *
+ *  Repl.it:https://repl.it/@bbeenn1227/ClassLoaderTest1
+ *  @Author bloodnighttw
+ *  
+ */
+
 import java.util.Scanner;
-import java.lang.*;
+
+import java.lang.ClassNotFoundException;
+import java.lang.InstantiationException;
+import java.lang.IllegalAccessException;
+
 class Main {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
+    System.out.print("Please enter the class' name u want to load?\n"
+                    +"This is the class list u can enter\n"
+                    +"=================\n"
+                    +"M1\n"
+                    +"M2\n"
+                    +"Main\n"
+                    +"Test\n"
+                    +"=================\n"
+                    +"Enter here:");
     
     try{
       Class<?> c=Class.forName("Test");
